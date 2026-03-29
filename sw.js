37a73d76ea36bd08837325c9082251dcc4bfc53c
pintofbmiguel-bit <pintofbmiguel@gmail.com>
2026-03-29 16:56:25 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'epee-log-v1';
+const ASSETS = [
+  '/epee-log/',
+  '/epee-log/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
